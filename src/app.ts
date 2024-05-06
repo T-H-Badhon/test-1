9977275aa46a0acce692359ef7cecfdd27f6889c
T-H-Badhon <thbadhons@gmail.com>
2024-05-06 17:43:07 +0600

    sfs

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-import router from "./routes/routes";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import router from './routes/routes'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send({
-    message: "server is running",
-  });
-});
+    message: 'server is rung',
+  })
+})
 
-app.use("/api/v1", router);
+app.use('/api/v1', router)
 
-export default app;
+export default app
